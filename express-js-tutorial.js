e6f17559497affdcb1b4db4643ebff475ee713c7
Punith <kpunith8@gmail.com>
2017-07-26 22:37:18 +0530

    Adding express and javascript content

---
+++ express-js-tutorial.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, '../src/index.html')); // Looks for index.html file src directory
+});
+
+app.listen(port, function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		open('http://localhost:' + port);
+	}
+});
+
+
+
+
